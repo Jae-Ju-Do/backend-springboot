ca4b2e3d8b9e6f3ae8cefef9f24292962f0f4724
standingash <tjswopark0123@gmail.com>
2025-01-03 16:37:18 +0900

    initial commit

---
+++ src/main/java/com/example/jaejudo/JaejudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaejudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaejudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaejudoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jaejudo/JaejudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaejudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaejudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

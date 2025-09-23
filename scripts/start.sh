b8c314aa4ba82381fe217e9ad074cec78fcaa25a
standingash <tjswopark0123@gmail.com>
2025-09-24 01:28:04 +0900

    환경변수 설정 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+source ~/.bashrc
+EnvironmentFile=/home/ec2-user/jaejudo/.env
+
 ROOT_PATH="/home/ec2-user/jaejudo"
-JAR="$ROOT_PATH/build/libs/application.jar"
+JAR_NAME="application.jar"
+JAR_SRC="$ROOT_PATH/build/libs/$JAR_NAME"
+JAR_DEST="$ROOT_PATH/$JAR_NAME"
 
 APP_LOG="/var/log/jaejudo/application.log"
 ERROR_LOG="/var/log/jaejudo/error.log"
@@ -12,11 +17,22 @@ NOW=$(date +%c)
 # 로그 디렉토리 생성 (없을 경우)
 mkdir -p /var/log/jaejudo
 
-# Java 경로 명시 (Amazon Linux에서 which java로 확인 가능)
+# JAR 파일 이동
+if [ -f "$JAR_SRC" ]; then
+    mv -f "$JAR_SRC" "$JAR_DEST"
+    echo "[$NOW] > $JAR_NAME 이동 완료: $JAR_SRC -> $JAR_DEST" >> $START_LOG
+else
+    echo "[$NOW] > 이동할 JAR 파일이 존재하지 않음: $JAR_SRC" >> $START_LOG
+    exit 1
+fi
+
+# Java 경로 명시
 JAVA_BIN=$(which java)
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup $JAVA_BIN -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+# 애플리케이션 실행
+echo "[$NOW] > $JAR_DEST 실행" >> $START_LOG
+nohup $JAVA_BIN -jar "$JAR_DEST" > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f $JAR)
+# 실행 PID 기록
+SERVICE_PID=$(pgrep -f "$JAR_NAME")
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
